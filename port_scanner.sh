2dd83be8b764b968c6d4295cf98dcc8640a1b371
ABHISHEK2k6 <abhishekp9a@gmail.com>
2024-11-25 21:34:14 +0530

    Update port_scanner.sh

--- port_scanner.sh
+++ port_scanner.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-# Port scanner script using nc (Netcat)
+# Default values
+ip=""
+ports=""
 
-# Check if the correct number of arguments is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <IP-ADDRESS>"
-  exit 1
-fi
-
-ip="$1"
-
-start_port=1
-end_port=100
-
-echo "Scanning ports on $ip..."
-
-
-for port in $(seq $start_port $end_port);
-do
-
-  nc -z -w1 $ip $port 2>/dev/null
-  if [ $? -eq 0 ]; then
-    echo "Port $port is open"
-  fi
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -i | --ip)
+            ip="$2"
+            shift
+            ;;
+        -p | --ports)
+            ports="$2"
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
 done
 
-echo "Scan completed on $ip."
+# Check if required options are provided
+if [[ -z $ip ]]; then
+    echo "Please specify an IP address with -i"
+    exit 1
+fi
+
+if [[ -z $ports ]]; then
+    echo "Please specify the max port range with -p"
+    exit 1
+fi
+
+# Scan and display open ports
+for ((i = 1; i <= ports; i++)); do
+    if (echo > /dev/tcp/$ip/$i) > /dev/null 2>&1; then
+        echo "$ip:$i is open"
+    fi
+done
